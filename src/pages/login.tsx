d32788246c0904cc524187dd76fc21cf809a9981
Sebastian Quiroga <sebastian.quiroga@ucp.edu.co>
2023-05-01 20:35:22 -0500

    Login feature implemented

---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import { Login } from "@/components/Login/Login";
+
+export default function Page() {
+  return <Login />;
+}
+
+// TODO: Work on routing and default routing
+// TODO: Work on sessions
+// TODO: Correct the login page route to just login/
